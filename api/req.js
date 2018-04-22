b97bc78b9484bc1bdeb3961d28d4270c777982aa
sak <saknarak@gmail.com>
2018-04-22 09:49:02 +0700

    add /req/major

--- api/req.js
+++ api/req.js
@@ -7,3 +7,12 @@ router.use('/trainee', require('./req-trainee'))
 // router.use('/a', require('./req-a'))
 // router.use('/b', require('./req-b'))
 // router.use('/c', require('./req-c'))
+router.get('/major', async (req, res) => {
+  let data = await req.db('major')
+    .orderBy('major_id')
+    .select(['major_id', 'major_name'])
+  res.send({
+    status: true,
+    data,
+  })
+})
